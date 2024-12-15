c5a2bb0cbe8d279c8473f37d6c316c6b1e0f0b7d
Anas Ahmad Abdelsalam <115808460+Anas-Ah25@users.noreply.github.com>
2024-12-15 03:53:17 +0200

    main app

--- starter.py
+++ starter.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 
 # Singleton Inventory Manager
 class InventoryManager:
+    _instance = None
     _inventory = {
         "Margherita": 10,
         "Pepperoni": 10,
@@ -10,6 +11,12 @@ class InventoryManager:
         "Olives": 10,
         "Mushrooms": 12,
     }
+
+    def __new__(cls): # added this to make it a singleton, where only one instance of the class is created
+        if cls._instance is None:
+            cls._instance = super(InventoryManager, cls).__new__(cls)
+        return cls._instance
+    
     def check_and_decrement(self, item: str) -> bool:
         if self._inventory.get(item, 0) > 0:
             self._inventory[item] -= 1
@@ -18,6 +25,68 @@ class InventoryManager:
 
     def get_inventory(self):
         return self._inventory
+
+# ----------  one pizze (core) ----------
+class Pizza(ABC):
+    @abstractmethod
+    def get_description(self):
+        pass
+
+    @abstractmethod
+    def get_cost(self):
+        pass
+
+# ----------- toppings ----------
+class topping(Pizza):
+    def __init__(self, pizza: Pizza):
+        self.pizza = pizza
+        self._inventory_manager = InventoryManager() 
+
+    def get_description(self):
+        return self.pizza.get_description()
+
+    def get_cost(self):
+        return self.pizza.get_cost()
+    def add_topping(self):
+        pass
+    
+class Cheese(topping):
+    def get_description(self):
+        if  self._inventory_manager.check_and_decrement("Cheese"):
+         return self.pizza.get_description() + ", Cheese"
+    def get_cost(self):
+      if self._inventory_manager.check_and_decrement("Cheese"):
+        return super().get_cost() + 1.0
+class Olives(topping):
+    def get_description(self):
+        if  self._inventory_manager.check_and_decrement("Olives"):
+         return self.pizza.get_description() + ", Olives"
+    def get_cost(self):
+      if self._inventory_manager.check_and_decrement("Olives"):
+        return super().get_cost() + 0.5
+class Mushrooms(topping):
+    def get_description(self):
+        if  self._inventory_manager.check_and_decrement("Mushrooms"):
+         return self.pizza.get_description() + ", Mushrooms"
+    def get_cost(self):
+        if self._inventory_manager.check_and_decrement("Mushrooms"):
+            return self.pizza.get_cost() + 0.7
+      
+    
+# ----------- types ----------
+class Margherita(Pizza):
+    def get_description(self):
+        return "Margherita Pizza"
+
+    def get_cost(self):
+        return 5.0
+
+class Pepperoni(Pizza):
+    def get_description(self):
+        return "Pepperoni Pizza"
+
+    def get_cost(self):
+        return 6.0
 
 
 
@@ -39,8 +108,17 @@ def main():
         if pizza_choice == '0':
             break
 
-        # TODO: Fill the pizza creation. 
+        
         pizza = None
+        if pizza_choice == "1":
+            pizza = Margherita()
+        elif pizza_choice == "2":
+            pizza = Pepperoni()
+        else:
+            print("This Pizza is unavailable")
+            continue
+
+
         # Add toppings
         while True:
             print("\nAvailable toppings:")
@@ -52,13 +130,13 @@ def main():
             # TODO: fill the following as required
             # Cheese
             if topping_choice == "1" :
-                pass
+                pizza = Cheese(pizza)
             # Olive
             elif topping_choice == "2":
-                pass
+                pizza = Olives(pizza)
             # Mushrooms
             elif topping_choice == "3":
-                pass
+                pizza = Mushrooms(pizza)
             elif topping_choice == "4":
                 break
             else:
@@ -76,3 +154,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
